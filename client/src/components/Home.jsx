12d0f5732f0d94a2372c1dcf472117494d3df9cd
roberto <mgroberto38@hotmail.com>
2023-08-01 10:19:52 -0500

    new

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,14 +7,21 @@ import img5 from "../assets/img6.jpg";
 
 const Home = () => {
   return (
-    <div className="bg-slate-400  bg-center h-screen">
+    <div className="bg-slate-400 bg-center h-screen flex flex-col justify-center items-center">
       <div className="h-96 w-full">
-        <img src={img} className="w-full h-full "></img>
+        <img src={img} className="w-full h-full object-cover" alt="Image 1" />
       </div>
-      <div>
-        <img src={img1} className="w-full h-full"></img>
+      <div className="grid grid-cols-3 gap-4 mt-4">
+        <img src={img1} className="w-full h-full object-cover" alt="Image 2" />
+        <img src={img2} className="w-full h-full object-cover" alt="Image 3" />
+        <img src={img3} className="w-full h-full object-cover" alt="Image 4" />
+      </div>
+      <div className="grid grid-cols-2 gap-4 mt-4">
+        <img src={img4} className="w-full h-full object-cover" alt="Image 5" />
+        <img src={img5} className="w-full h-full object-cover" alt="Image 6" />
       </div>
     </div>
   );
 };
+
 export default Home;
