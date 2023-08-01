98c7e18e53bca6db77a41e67298ed72f0028f7f9
roberto <mgroberto38@hotmail.com>
2023-08-01 10:23:15 -0500

    new

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -2,32 +2,47 @@ import { Link } from "react-router-dom";
 
 const Form = () => {
   return (
-    <div className="bg-slate-500 bg-cover mx-auto h-screen">
-    
-    
-      <div className="flex justify-center px-10 py-10">
-      
-        <form className="bg-white border-4 px-4 py-4">
-        <ul className="text-center px-4">
-          <li className="inline px-4 bg-red-500">Login</li>
-          <li className="inline bg-yellow-400">Register</li>
+    <div className="bg-slate-500 bg-cover h-screen flex justify-center items-center">
+      <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-6">
+        <ul className="flex justify-center mb-4">
+          <li className="px-4 py-2 bg-red-500 rounded-tl-lg text-white font-bold">
+            <Link to="/login">Login</Link>
+          </li>
+          <li className="px-4 py-2 bg-yellow-400 rounded-tr-lg text-white font-bold">
+            <Link to="/register">Register</Link>
+          </li>
         </ul>
-          <div className="px-4 py-4 bg-white">
+        <form className="space-y-4">
+          <div className="flex flex-col">
+            <label className="text-black font-bold mb-2">Email</label>
             <input
-              className="border-2 h-9 w-64 text-black font-bold text-center"
+              className="border-2 border-black rounded px-4 py-2 focus:outline-none focus:border-blue-500"
               type="text"
               placeholder="Email"
-            ></input>
+            />
           </div>
-          <div className="px-4 py-4 ">
+          <div className="flex flex-col">
+            <label className="text-black font-bold mb-2">Password</label>
             <input
-              className=" border-2 border-slate-500 placeholder:text-black selection:bg-slate-400 text-black  font-bold text-center"
+              className="border-2 border-black rounded px-4 py-2 focus:outline-none focus:border-blue-500"
               type="password"
               placeholder="Password"
-            ></input>
+            />
           </div>
-          <Link to="/dashboard">Submit</Link>
-          <Link to="/newdata">new</Link>
+          <div className="flex justify-between items-center">
+            <Link
+              to="/dashboard"
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Submit
+            </Link>
+            <Link
+              to="/newdata"
+              className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              New
+            </Link>
+          </div>
         </form>
       </div>
     </div>
@@ -35,3 +50,4 @@ const Form = () => {
 };
 
 export default Form;
+
